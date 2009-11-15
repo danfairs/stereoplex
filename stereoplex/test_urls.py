ed72abdba70ab74b63d21424560368697313a226
Dan Fairs <dan@fezconsulting.com>
2009-11-15 16:08:36 +0000

    Skeleton from fez.djangoskel django_app template

---
+++ stereoplex/test_urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+# URL test patterns for stereoplex. Use this file to ensure a consistent
+# set of URL patterns are used when running unit tests. This test_urls
+# module should be referred to by your test class.
+
+urlpatterns = patterns('stereoplex.views',
+  # Add url patterns here
+)
---
+++ stereoplex/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import *
+
+# URL patterns for stereoplex
+
+urlpatterns = patterns('stereoplex.views',
+  # Add url patterns here
+)
