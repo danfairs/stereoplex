bab62d02c4bb7f67dc5c976d586fecc2d66c9634
Dan Fairs <dan@fezconsulting.com>
2009-11-30 11:31:31 +0000

    Note that markdown is allowed.

--- stereoplex/forms.py
+++ stereoplex/forms.py
@@ -11,3 +11,4 @@ class CommentCaptchaForm(CommentForm):
     def __init__(self, *args, **kwargs):
         super(CommentCaptchaForm, self).__init__(*args, **kwargs)
         self.fields['email'].help_text = _(u"This won't be published anywhere, it's just in case I need to contact you.")
+        self.fields['comment'].help_text = _(u"You can use Markdown in your comments. Be sensible!")
