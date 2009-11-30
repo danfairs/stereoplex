6830211a7d4223e38e1d42e606dd33f41f3aadb8
Dan Fairs <dan@fezconsulting.com>
2009-11-30 11:29:28 +0000

    Add help_text for a couple of fields.

--- stereoplex/forms.py
+++ stereoplex/forms.py
@@ -1,6 +1,13 @@
 from stereoplex.fields import ReCaptchaField
 from django.contrib.comments.forms import CommentForm
+from django.utils.translation import ugettext_lazy as _
 
 class CommentCaptchaForm(CommentForm):
-    recaptcha = ReCaptchaField()
-    
+    recaptcha = ReCaptchaField(
+        label=_(u"Stop the Spam!"),
+        help_text=_(u"Sorry about this, but I don't want spam comments.")
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(CommentCaptchaForm, self).__init__(*args, **kwargs)
+        self.fields['email'].help_text = _(u"This won't be published anywhere, it's just in case I need to contact you.")
