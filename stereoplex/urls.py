47a4adb543eac6d603b6fd85176cbef1196e5259
Dan Fairs <dan@fezconsulting.com>
2009-12-29 21:10:37 +0000

    Add an all posts view

--- stereoplex/urls.py
+++ stereoplex/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls.defaults import *
-from mingus.urls import urlpatterns
+from mingus.urls import urlpatterns as mingus_urlpatterns
+
+urlpatterns = patterns('stereoplex.views',
+    url(r'^all/$', 'all', name='stereoplex_all')
+) + mingus_urlpatterns
 
 urlpatterns += patterns('',
     (r'^tinymce/', include('tinymce.urls')),
--- stereoplex/views.py
+++ stereoplex/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.views.generic import list_detail
+from django_proxy.models import Proxy
+from view_cache_utils import cache_page_with_prefix
+from mingus.core.views import page_key_prefix
+
+@cache_page_with_prefix(60, page_key_prefix)
+def all(request, page=0, template_name='proxy/proxy_list.html', **kwargs):
+    '''
+    Homepage.
+
+    Template: ``proxy/proxy_list.html``
+    Context:
+        object_list
+            Aggregated list of Proxy instances (post, quote, bookmark).
+
+    '''
+
+    posts = Proxy.objects.published().order_by('-pub_date')
+
+    return list_detail.object_list(
+        request,
+        queryset = posts,
+        template_name = template_name,
+        **kwargs
+    )
