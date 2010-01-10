a2080a915689c4ecb49809810c29935cbdfae88d
Dan Fairs <dan@fezconsulting.com>
2010-01-10 17:55:31 +0000

    Add the code style to TinyMCE

--- stereoplex/admin.py
+++ stereoplex/admin.py
@@ -20,7 +20,8 @@ class TinyPostAdmin(PostAdmin):
                     'theme_advanced_toolbar_location' : "top",
                     'theme_advanced_toolbar_align' : "left",
                     'theme_advanced_statusbar_location' : "bottom",
-                    'theme_advanced_resizing' : True,                    
+                    'theme_advanced_resizing' : True,               
+                    'theme_advanced_styles': 'Code=code',     
                     },
             ))
         return super(TinyPostAdmin, self).formfield_for_dbfield(db_field, **kwargs)
